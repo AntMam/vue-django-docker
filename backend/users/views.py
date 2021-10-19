3dabb6c22fda0bd1f1779f186eb03b125ac83519
AntMam <anthony.mameli@gmail.com>
2021-10-19 18:19:30 -0400

    added base user and authentication

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from rest_framework import exceptions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from . models import User
+from . serializers import UserSerializer
+from . authentication import generate_access_token, JWTAuthentication
 
-# Create your views here.
+@api_view(['GET'])
+def register(request):
+    data = request.data
+    if data['password'] != data['password_confirm']:
+        raise exceptions.APIException('Passwords do not match.')
+    serializer = UserSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def login(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+
+    user = User.objects.filter(email=email).first()
+    if user is None:
+        raise exceptions.AuthenticationFailed('User not found.')
+
+    if not user.check_password(password):
+        raise exceptions.AuthenticationFailed('Incorrect password')
+    
+    response = Response()
+
+    token = generate_access_token(user)
+    response.set_cookie(key='jwt', value=token, httponly=True)
+    response.data = {
+        'jwt': token
+    }
+
+    return response
+
+@api_view(['POST'])
+def logout(_):
+    response = Response()
+    response.delete_cookie(key='jwt')
+    response.data = {
+        'message': 'Logout Success'
+    }
+
+    return response
+    
+class AuthenticatedUser(APIView):
+    
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+
+        return Response({
+            'data': serializer.data
+        })
+
+@api_view(['GET'])
+def users(request):
+    serializer = UserSerializer(User.objects.all(), many=True)
+    return Response(serializer.data)
